920ffed7ffef7174cf367c4b67fae38cbafc6c04
WillWSmith <will.w.smith94@gmail.com>
2024-05-04 10:38:32 -0400

    add logic to benchmark method

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,6 +1,11 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield # This executes the block passed to the method
+  end_time = Time.now
+  running_time = end_time - start_time
+  running_time # Return the running time
 end
+
  
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
